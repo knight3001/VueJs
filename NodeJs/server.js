8cb092f67c4f2eb7a5c75a42de529e23d30a9eda
Terry <knight3001@aliyun.com>
2017-07-20 15:22:27 +1000

    more node js prac

--- NodeJs/server.js
+++ NodeJs/server.js
@@ -5,13 +5,16 @@ var fs = require('fs');
 http.createServer(function (req, res) {
   var q = url.parse(req.url, true);
   var filename = "." + q.pathname;
+  console.log("Request for " + filename + " received.");
   fs.readFile(filename, function(err, data) {
     if (err) {
       res.writeHead(404, {'Content-Type': 'text/html'});
       return res.end("404 Not Found");
     }  
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
+    res.write(data.toString());
+    res.end();
   });
 }).listen(8080);
+
+console.log('Server running at http://127.0.0.1:8080/');
